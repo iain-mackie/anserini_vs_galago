ee928cb0271f4ad8fce27f4b8f5a0e645ce29915
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-02-19 14:54:40 +0000

    pushing first add

--- analysis.py
+++ analysis.py
@@ -14,18 +14,14 @@ if __name__ == '__main__':
     df = pd.read_csv(os.path.join(os.getcwd(), 'df.csv'))
 
     df_metrics = get_biggest_diff(df=df, col='anserini_best', metric='map')
-
     df_metrics.to_csv(os.path.join(os.getcwd(), 'map_diff.csv'), index=False)
 
     abs_diff = 0.03
-
     inputs = df_metrics[df_metrics['abs_diff'] > abs_diff]['anserini_best']
-
     n = len(inputs) / len(df_metrics)
 
     # the histogram of the data
     plt.hist(inputs, 50, density=False, facecolor='g', alpha=0.75)
-
     plt.ylabel('Count -> {:.2f}% of data i.e. queries with abs MAP diff > {:.2f}'.format(n, abs_diff))
     plt.xlabel('Anserini MAP minus Galago MAP')
     plt.title('Anserini vs. Galago - histogram of tree-hierarchical (abs diff > {:.2f})'.format(abs_diff))
